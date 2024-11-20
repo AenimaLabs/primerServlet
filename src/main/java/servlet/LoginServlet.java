58c185d871867e4c6f48b13ff02354c28eefe24d
AenimaLabs <aenimalabs@gmail.com>
2024-11-19 21:57:41 -0300

    Login Logout

---
+++ src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String usuario = req.getParameter("usuario");
+        String contrasena = req.getParameter("contrasena");
+
+        if ("admin".equals(usuario) && "admin".equals(contrasena)) {
+            resp.sendRedirect("Bienvenida.jsp");
+        } else {
+            resp.sendRedirect("index.jsp?error=1");
+        }
+    }
+}
